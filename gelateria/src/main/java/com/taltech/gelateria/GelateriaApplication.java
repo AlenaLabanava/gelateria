08a03db72c794b6d73869bb3917da22a558d1f62
a s u s <AlenaLabanava>
2019-05-15 16:31:33 +0300

    Initial commit

---
+++ gelateria/src/main/java/com/taltech/gelateria/GelateriaApplication.java
@@ -0,0 +1,13 @@
+package com.taltech.gelateria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GelateriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GelateriaApplication.class, args);
+	}
+
+}
