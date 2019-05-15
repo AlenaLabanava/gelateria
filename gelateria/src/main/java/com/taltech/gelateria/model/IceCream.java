32d8ce01e091932000efb0bd1c888c59936cb3f9
user <misskiriko97@gmail.com>
2019-05-15 17:13:22 +0300

    Entity added

---
+++ gelateria/src/main/java/com/taltech/gelateria/model/IceCream.java
@@ -0,0 +1,61 @@
+package com.taltech.gelateria.model;
+
+import io.swagger.models.auth.In;
+
+import javax.annotation.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class IceCream {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.AUTO)
+    private Long id;
+    private String type;
+    private String taste;
+    private Integer price;
+
+    public IceCream(){
+
+    }
+    public IceCream (String type, String taste, Integer price){
+        this.type = type;
+        this.taste = taste;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getTaste() {
+        return taste;
+    }
+
+    public void setTaste(String taste) {
+        this.taste = taste;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+}
